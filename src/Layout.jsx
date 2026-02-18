39d283473390466d5611a8c914b0f0c7ea012fdb
Nadav Levy <nadavlevy40@gmail.com>
2026-02-18 11:01:33 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,6 @@
 export default function Layout({ children }) {
   return (
-    <div className="w-full h-screen overflow-hidden">
+    <div className="w-full min-h-screen overflow-y-auto">
       {children}
     </div>
   );
